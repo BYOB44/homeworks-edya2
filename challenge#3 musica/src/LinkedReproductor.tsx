f565f890354117a70228ec1724c9b3e08afde054
acercandro <alemol0615@gmail.com>
2026-02-28 15:49:49 -0500

    Add more songs to the mockedSongs array in LinkedReproductor

--- challenge#3 musica/src/LinkedReproductor.tsx
+++ challenge#3 musica/src/LinkedReproductor.tsx
@@ -14,6 +14,11 @@ const mockedSongs: Song[] = [
   { id: 3, title: "Thirty Seconds To Mars", artist: "The Kill" },
   { id: 4, title: "Estrella Roja", artist: "3am, Andrea Ferrero" },
   { id: 5, title: "You only live once", artist:"Suicide Silence"},
+  { id: 6, title: "Given up", artist:"linkin park"},
+  { id: 7, title: "Back in black", artist:"AC/DC"},
+  { id: 8, title: "The resistance", artist:"Skillet"},
+  { id: 9, title: "UNA CANCION BONITA", artist:"3am, yami safdle"},
+  { id: 10, title: "I wont give up", artist:"Jason Mraz"},
 ];
 
 type PlayerState = {
